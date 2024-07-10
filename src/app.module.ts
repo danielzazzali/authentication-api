4a2b4da589e0dc171d9e248b19119e83ba82ba82
Daniel Zazzali <dazazzali@gmail.com>
2024-07-09 23:05:36 -0400

    Delete ugly code

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { UserModule } from './user/user.module';
 import { NotificationService } from './notification/notification.service';
 import { LoggingMiddleware } from './middleware/logging.middleware';
 import { TokenService } from './token/token.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [AuthModule, DatabaseModule, UserModule],
-  controllers: [],
-  providers: [NotificationService, TokenService],
+  controllers: [AppController],
+  providers: [AppService, NotificationService, TokenService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
